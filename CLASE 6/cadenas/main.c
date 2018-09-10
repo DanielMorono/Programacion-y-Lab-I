0bcd0b595cdcf5b21206cf83434830ccb3266fd8
Daniel <morono.daniel99@gmail.com>
2018-09-10 15:20:54 -0300

    clase terminada

---
+++ CLASE 6/cadenas/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //incluir siempre que se trabaje con cadenas
+
+int main()
+{
+    char cadena[20]="perro";//siempre tiene que tener un elemento mas
+    char cadena2[50]="gato";
+    int cantidad;
+    int estado;
+
+strcat(cadena2,cadena);
+
+//strlwr(cadena2);
+
+puts(cadena2);
+
+    //printf("%d",estado);
+
+
+
+
+//    strcpy(cadena,"gato");
+
+
+    //cantidad = strlen(cadena);
+
+   // printf("%d\n", cantidad);
+
+
+
+    /*printf("Ingrese texto: \n");
+    fflush(stdin);
+    // scanf("%s", &cadena);
+    gets(cadena);
+    printf("%s",cadena);
+
+*/
+    return 0;
+}
--- CLASE 6/practica/main.c
+++ CLASE 6/practica/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int inicializarVector(int[],int,int);//Siempre que tenga que pasar un vector hay que pasar estos dos parametros minimo
 int mostrarVector(int[], int);
 int main()
 {
-   int miArrayDelMain[100];
-   inicializarVector(miArrayDelMain,100,-1);
-   mostrarVector(miArrayDelMain,100);
+   int miArrayDelMain[10];
+   inicializarVector(miArrayDelMain,10,-1);
+   mostrarVector(miArrayDelMain,10);
 
     return 0;
 }
