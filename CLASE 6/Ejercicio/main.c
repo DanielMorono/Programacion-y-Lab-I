bd2ff1c4064c5ab2935389a5eb2efe3d8e08d5ff
Daniel <morono.daniel99@gmail.com>
2018-09-11 10:39:01 -0300

    Apuntes

--- CLASE 6/Ejercicio/main.c
+++ CLASE 6/Ejercicio/main.c
@@ -1,55 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main()
 {
-    char nombre[21];
-    char apellido[21];
-    int cantidadCaracteresNombre;
-    int cantidadCaracteresApellido;
+    char nombre[20];
+    char apellido[20];
+    char nombreCompleto[41]; // "";
+    char buffer [1024];
+    int i;
 
     printf("Ingrese su nombre: \n");
 
     fflush(stdin);
-    gets(nombre);
-    cantidadCaracteresNombre = strlen(nombre);
+    gets(buffer);
 
-    while (cantidadCaracteresNombre >20)
+    while (strlen(buffer) > 19)
     {
         system("pause");
         system("cls");
         printf("El nombre es muy largo, ingrese un nombre valido: \n");
         fflush(stdin);
-        gets(nombre);
-        cantidadCaracteresNombre = strlen(nombre);
+        gets(buffer);
     }
 
+    strcpy(nombre,buffer);
     system("pause");
     system("cls");
 
     printf("Ingrese su apellido: \n");
 
     fflush(stdin);
-    gets(apellido);
-    cantidadCaracteresApellido = strlen(apellido);
+    gets(buffer);
 
-     while(cantidadCaracteresApellido>20)
+
+     while(strlen(buffer) > 19)
     {
         system("pause");
         system("cls");
         printf("El apellido es muy largo, ingrese un apellido valido: \n");
         fflush(stdin);
-        gets(apellido);
-        cantidadCaracteresApellido = strlen(apellido);
+        gets(buffer);
     }
+
+    strcpy(apellido,buffer);
+
+    strcpy(nombreCompleto,apellido);
+    strcat(nombreCompleto, ", ");
+    strcat(nombreCompleto, nombre);
+
+    strlwr(nombreCompleto);
+
+    nombreCompleto[0]=toupper(nombreCompleto[0]);
+
+    for(i=0; i< strlen(nombreCompleto); i++)
+    {
+        if(nombreCompleto[i]== ' ') // isspace(nombreCompleto[1]))
+            {
+                nombreCompleto[i+1] = toupper(nombreCompleto[i+1]);
+            }
+    }
+
+
+    puts(nombreCompleto);
 
     system("pause");
     system("cls");
 
-    printf("%s\n",nombre);
-    printf("%s\n",apellido);
-    printf("%d\n",cantidadCaracteresNombre);
-    printf("%d\n",cantidadCaracteresApellido);
     return 0;
 }
---
+++ CLASE 7/practica/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+//void insertion(int data[],int len);
+int main()
+{
+    int legajos[10];
+    char nombres [10][21];
+    int notas[10];
+
+    return 0;
+}
+
+/*void insertion(int data[],int len)
+    {
+        int i,j;
+        int temp;
+        for (i=1; i<len; i++)
+        {
+            temp = data [i];
+            j=i-1;
+            while (j>=0 && temp<data[j])
+            {
+                data [j+1] = data [j];
+                j--;
+            }
+            data [j+1]=temp; //insercion
+        }
+    {
+        int i,j;
+        int temp;
+        for (i=1; i<len; i++)
+        {
+            temp = data [i];
+            j=i-1;
+            while (j>=0 && temp<data[j])
+            {
+                data [j+1] = data [j];
+                j--;
+            }
+            data [j+1]=temp; //insercion
+        }*/
